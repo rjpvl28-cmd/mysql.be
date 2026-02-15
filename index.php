c7f70ee7b35bcd052375ea5173dd2b650bcd8459
itschool <itschool@tou.edu.kz>
2026-02-15 12:02:29 +0500

    1

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
+
+    $database = "mydatabase";
+    $host = "127.0.0.1";
+    $port = "3306";
+    $user = "root";
+    $pass = "";
+    $dsn = "mysql:dbname={$database};host={$host};port={$port}";
+    $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+    $connection = new PDO($dsn, $user, $pass, $options);
+
+if ($_SERVER[`REQUEST_METHOD`] == `GET`) {
+    $sql = 'SELECT * FROM `users`';
+    $query = $connection->query($sql);
+    $users = $query->fetchAll();
+    
     echo json_encode([
-        'title' => 'Главная страница'
-    ]);
+        'title' => 'Главная страница',
+        'users' => $users
+    ]); 
+}
+
+if ($_SERVER[`REQUEST_METHOD`] == `POST`) {
+    $raw = file_get_contents('php://input');
+    $data = json_decode($raw, true);
+    print_r(data);
+}
+    
