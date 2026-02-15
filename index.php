d883079a77a73304bafb72ebe87e16057546802e
itschool <itschool@tou.edu.kz>
2026-02-15 09:20:32 +0500

    1

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php 
-    header("Access-Control-Allow-Origin: http://localhost:5173");
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit;
-    }
-    
+<?php
     echo json_encode([
         'title' => 'Главная страница'
     ]);
