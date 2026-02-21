822f2c7620d16af2a83752efe2570fb75cf1c0dd
itschool <itschool@tou.edu.kz>
2026-02-21 12:02:44 +0500

    1

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     ];
     $connection = new PDO($dsn, $user, $pass, $options);
 
-if ($_SERVER[`REQUEST_METHOD`] == `GET`) {
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $sql = 'SELECT * FROM `users`';
     $query = $connection->query($sql);
     $users = $query->fetchAll();
@@ -20,11 +20,50 @@ if ($_SERVER[`REQUEST_METHOD`] == `GET`) {
         'title' => 'Главная страница',
         'users' => $users
     ]); 
+    die();
+
 }
 
-if ($_SERVER[`REQUEST_METHOD`] == `POST`) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $raw = file_get_contents('php://input');
     $data = json_decode($raw, true);
-    print_r(data);
+    // print_r(data);
+    $sql = 'INSERT INTO `users` (`firstname`, `secondname`, `age`) 
+            VALUES (?, ?, ?)';
+    $query = $connection->prepare($sql);
+    $result = $query->execute([
+        $data['firstname'],
+        $data['secondname'],
+        $data['age'],
+    ]);
+
+    echo json_encode([
+        'sucess' => true,
+    ]);
+    exit(); 
+}
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $sql = 'DELETE FROM `users` WHERE `id` = :id';
+    $query = $connection->prepare($sql);
+    $query->execute([
+        'id' => $_GET['id']
+    ]);
+    echo json_encode([
+        'sucess' => true
+    ]);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'UPDATE') {
+    $raw = file_get_contents('php://input');
+    $data = json_decode($raw, true);
+    $sql = 'UPDATE `users` SET `firstname` = ?, `age` = ?, WHERE `id` = ?';
+    $query->execute([
+        $data['firstname'],
+        $data['secondname'],
+        $data['age'],
+        $data['id']
+    ]);
+     echo json_encode([
+        'sucess' => true
+    ]);
 }
     
